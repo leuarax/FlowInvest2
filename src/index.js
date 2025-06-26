6fa59dcd2ad096d57144a2f8e3b14424f4c2d053
leuarax <63297831+leuarax@users.noreply.github.com>
2025-06-26 19:08:40 +0200

    fix: wrap routes in BrowserRouter to resolve white screen and enable routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
 import AddRealEstate from './components/AddRealEstate';
-import { Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Routes>
-      <Route path="/add-real-estate" element={<AddRealEstate />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/add-real-estate" element={<AddRealEstate />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
