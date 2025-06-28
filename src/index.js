71084c054cd5a9df5c31fdc5bf3a6f443a403a98
leuarax <63297831+leuarax@users.noreply.github.com>
2025-06-28 19:06:02 +0200

    Add top-level console log to check Supabase env variables at runtime

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+console.log('ENV TEST:', process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
